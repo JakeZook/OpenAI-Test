f9ab0bc2e74a98a3398aaacc83e74c73828d7f1b
Jake Zook <jakezook01@gmail.com>
2023-10-28 20:00:36 -0600

    Added user input.

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 import { config } from "dotenv";
 config();
+
 import { OpenAI } from "openai";
+import readline from "readline";
 
 const openai = new OpenAI({ apiKey: process.env.API_KEY });
 
-openai.chat.completions
-	.create({
+const userInterface = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+userInterface.prompt();
+userInterface.on("line", async (input) => {
+	const res = await openai.chat.completions.create({
 		model: "gpt-3.5-turbo",
-		messages: [{ role: "user", content: "Hello ChatGPT" }],
-	})
-	.then((res) => {
-		console.log(res.choices[0].message.content);
-		// res.choices.forEach((out) => console.log(out.message));
+		messages: [{ role: "user", content: input }],
 	});
+	console.log(res.choices[0].message.content);
+	userInterface.prompt();
+});
